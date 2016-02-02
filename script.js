b9078350c8aa68bac1c2ae63399499454c315549
Anton Tatuev <tatuev.a.a@yandex.ru>
2016-02-02 22:16:04 +0300

    добавил расплывчатость

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ myBlurFunction = function(state) {
         containerElement.setAttribute('class', null);
     }
 };
+
+myBlurFunction2 = function(state) {
+    if (state) {
+        document.getElementById('example').className='alerting';
+        document.getElementById('overlay').style.display='block';
+    } else {
+        document.getElementById('example').className='';
+        document.getElementById('overlay').style.display='none';
+    }
+
+    /* state can be 1 or 0 
+    var containerElement = document.getElementById('main_container');
+    var overlayEle = document.getElementById('overlay');
+
+    if (state) {
+        overlayEle.style.display = 'block';
+        containerElement.setAttribute('class', 'blur');
+    } else {
+        overlayEle.style.display = 'none';
+        containerElement.setAttribute('class', null);
+    } */
+};
