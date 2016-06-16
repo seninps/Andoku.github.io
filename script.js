4cea460f5c0c54aca3072bb5bbb4db5b510736e0
Andrushka36 <loko@progtech.ru>
2016-06-16 22:27:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ hide = function(type) {
 	if (text.style.display == 'none') {
 		text.style.display = 'block';
 		document.getElementById('popup').style.height = '850px';
-		document.getElementById('popup').className = document.getElementById('about').style.height;
+		document.getElementById('popup').className = document.getElementById('about').offsetheight;
 		if (document.getElementById('about').style.height < '1010px') document.getElementById('about').style.height='1010px';
 	} else {
 		text.style.display = 'none';
