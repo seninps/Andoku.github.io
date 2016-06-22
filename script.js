6e869083ef6f784b574536075956ab06840ebb52
Andrushka36 <loko@progtech.ru>
2016-06-22 15:12:36 +0300

    поправил функцию в блоке "о продукте"

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ myBlurFunction2 = function(state) {
 };
 
 product-category = function(type) {
-	if (type == components) { 
+	if (type == 'components') { 
 		document.getElementById('why-we-images').style.display='none';
 		document.getElementById('components-images').style.display='block';	
 	} else {
