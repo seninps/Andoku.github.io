4f05e6070a12a850ca07325d40125acc61ae256f
Andrushka36 <loko@progtech.ru>
2016-06-22 16:49:38 +0300

    поправил отображение блоков в разделе "о продукте"

--- script.js
+++ script.js
@@ -36,6 +36,8 @@ myBlurFunction2 = function(state) {
         document.getElementById('blur-div').className='alerting';
         document.getElementById('product').style.display='block';
         document.getElementById('background').style.display='block';
+        document.getElementById('why-we-images').style.display='block';
+	document.getElementById('healthy').style.display='block';
     } else if (state == 5) {
         document.getElementById('blur-div').className='alerting';
         document.getElementById('photo').style.display='block';
@@ -61,11 +63,6 @@ myBlurFunction2 = function(state) {
         overlayEle.style.display = 'none';
         containerElement.setAttribute('class', null);
     } */
-};
-
-document.ready = function(){
-	document.getElementById('why-we-images').style.display='block';
-	document.getElementById('healthy').style.display='block';
 };
 
 ProductCategory = function(type) {
