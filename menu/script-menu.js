c76b921e658dee09640faf76d66bf05f51c74073
Andrushka36 <loko@progtech.ru>
2016-06-26 22:07:02 +0300

    изменил функцию скрытия крестика

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -1,8 +1,9 @@
 document.ready = function() {
   document.getElementById('menu').style.display='block';
-};
-
-document.onclick = function() {  
-  if (document.getElementById('product-1').style.display=='block') document.getElementById('close').style.display='none';
-  else document.getElementById('close').style.display='block';
+  
+  $(document).ready(function(){
+    $(".image").click(function(){
+			$(".close-icon").hide();
+		});
+  });
 };
