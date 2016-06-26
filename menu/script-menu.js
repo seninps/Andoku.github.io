53bc5b39a9775285480e3931a978c4e762dee498
Andrushka36 <loko@progtech.ru>
2016-06-26 21:46:45 +0300

    добавил убирание крестика

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -1,3 +1,6 @@
 document.ready = function() {
   document.getElementById('menu').style.display='block';
+  
+  if (document.getElementById('product-1').style.display='block') document.getElementsByClassName('close-icon').style.display='none';
+  else document.getElementsByClassName('close-icon').style.display='block';
 };
