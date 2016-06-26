1f2ff842aa679d478027d83141a2d6830bb023cc
Andrushka36 <loko@progtech.ru>
2016-06-26 21:49:45 +0300

    поправил функцию скрытия большого крестика

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -1,6 +1,8 @@
 document.ready = function() {
   document.getElementById('menu').style.display='block';
-  
+};
+
+document.onclick = function() {  
   if (document.getElementById('product-1').style.display='block') document.getElementsByClassName('close-icon').style.display='none';
   else document.getElementsByClassName('close-icon').style.display='block';
 };
