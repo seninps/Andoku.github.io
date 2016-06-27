981305d5209599b43b79f7cb7c40da96779c590c
Andrushka36 <loko@progtech.ru>
2016-06-27 15:24:50 +0300

    поправил функцию смены картинки в первом блоке

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -7,11 +7,11 @@ document.ready = function() {
 	});
 	
 	$("#product-1 .content").hover(function(){
-		$("#product-1 .product-image").css(("background-color", "url(/menu/1/content.jpg)");
+		$("#product-1 .product-image").css("background-color", "url(/menu/1/content.jpg)");
 	});
 	
 	$("#product-1 .vitamins").hover(function(){
-		$("#product-1 .product-image").css(("background-color", "url(/menu/1/vitamins.jpg)");
+		$("#product-1 .product-image").css("background-color", "url(/menu/1/vitamins.jpg)");
 	});
 	
 	$("#product-1 .main").hover(function(){
