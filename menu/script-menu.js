adce646f48d087bd2879113330e81b8ff554321d
Andrushka36 <loko@progtech.ru>
2016-06-27 15:09:23 +0300

    добавил функцию смены картинок

--- menu/script-menu.js
+++ menu/script-menu.js
@@ -2,9 +2,22 @@ document.ready = function() {
   document.getElementById('menu').style.display='block';
   
   $(document).ready(function(){
-    $(".image").click(function(){
+  	$(".image").click(function(){
 			$(".close-icon").hide();
-		});
+	});
+	
+	$("#product-1 .content").hover(function(){
+		$("product-image").css(("background-color", "url(/menu/1/content.jpg)");
+	});
+	
+	$("#product-1 .vitamins").hover(function(){
+		$("product-image").css(("background-color", "url(/menu/1/vitamins.jpg)");
+	});
+	
+	$("#product-1 .main").hover(function(){
+		$("product-image").css("background-color", "url(/menu/1/main.jpg)");
+	});
+  	
   });
   
 };
