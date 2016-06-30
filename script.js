1d03af97de69590414f167ef267b8b91c54323e5
Andrushka36 <loko@progtech.ru>
2016-06-30 21:42:09 +0300

    поправил функцию возвращения в блоки "о нас" и "о продукте"

--- script.js
+++ script.js
@@ -132,12 +132,12 @@ myBlurFunction2 = function(state) {
 		if (document.getElementById('product').style.display=='block') {
 	    		document.getElementById('product').style.display='none';
 	    		document.getElementById('product-background').style.display='none';
-	        	document.getElementById('product-background').className='product';
+	        	document.getElementById('product-background').className='';
 	    	};
 	    	if (document.getElementById('about').style.display=='block') {
 	    		document.getElementById('about').style.display='none';
 	    		document.getElementById('product-background').style.display='none';
-	        	document.getElementById('product-background').className='about';
+	        	document.getElementById('product-background').className='';
 	        	player.pauseVideo();
 	    	};
 	}
