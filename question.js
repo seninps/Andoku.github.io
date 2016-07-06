e500207ae39d07c1713759a6078bcff3f20e2fc1
Andrushka36 <loko@progtech.ru>
2016-07-06 18:34:46 +0300

    Delete question.js

--- question.js
+++
@@ -1,7 +0,0 @@
-document.ready = function() {
-	$(document).ready(function(){
-		$(".send").click(function(){
-			$(this).load("question.php","&name="+$(".name").val()+"&email="+$(".email").val()+"&message="+$(".message").val());
-		})
-	});   
-};
