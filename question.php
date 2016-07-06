e627f6981bb97b50cf8bb3fa7b612f58f6f98b94
Andrushka36 <loko@progtech.ru>
2016-07-06 18:18:59 +0300

    вопрос

---
+++ question.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+	$to = 'r-snack@mail.ru';
+	$subject = 'R-snack Question';
+	$message = '
+Имя: '.$_REQUEST['name']."<br>".'
+E-mail: '.$_REQUEST['email']."<br>".'
+Текст вопроса: '.$_REQUEST['message'];
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
+	mail($to, $subject, $message, $headers);
+	
+?>
