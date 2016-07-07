432de228cfd354aaf82af66f8fb05aef8d0b28bf
Andrushka36 <loko@progtech.ru>
2016-07-07 17:03:53 +0300

    поправил функцию скрытого текста

--- script.js
+++ script.js
@@ -285,14 +285,14 @@ hide = function(type) {
 	if (type == 'price') {
 		if (text.style.display == 'none') {
 			text.style.display = 'block';
-			document.getElementByClassName('principles').className='revealed';
+			document.getElementsByClassName('principles').className='revealed';
 			/*height = document.getElementById('popup').offsetHeight + 72;
 			document.getElementById('popup').style.height = height + 'px';
 			height = document.getElementById('about').offsetHeight + 72;
 			document.getElementById('about').style.height = height + 'px';*/
 		} else {
 			text.style.display = 'none';
-			document.getElementByClassName('principles').className=''
+			document.getElementsByClassName('principles').className=''
 			/*height = document.getElementById('popup').offsetHeight - 72;
 			document.getElementById('popup').style.height = height + 'px';
 			height = document.getElementById('about').offsetHeight - 72;
